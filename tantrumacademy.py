c6ffd986b9cbbe405ce1c02672a7b6dc9ae733cc
shubham4432 <43362205+shubham4432@users.noreply.github.com>
2018-09-18 09:53:27 +0530

    update2

--- tantrumacademy.py
+++ tantrumacademy.py
@@ -1,9 +1,8 @@
-a=10
-b=20
+a=int(input("enter the first no"))
+b=int(input("enter the second no"))
 if b>0:
 print(b,"is greater than",a)
 elif:
 print(a,"is greater than",b)
 else:
 print(a,"is equal to",b)
-a=int(input("enter the no."))
